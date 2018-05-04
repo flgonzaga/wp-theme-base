6b21cb5af6687a24fc99769dfabe79e893895a05
Web01 <fabio.gonzagabr@gmail.com>
2018-05-04 09:53:43 -0300

    update banner

--- banner.php
+++ banner.php
@@ -1,5 +1,3 @@
-<section id="banner" class="w-100 hidden-sm-down">
-	<div class="owl-banner owl-carousel owl-theme fadeIn animated">
-		<a href="" class="item d-inline-block" style="background-image: url(http://via.placeholder.com/2560x690);"></a>
-	</div>
-</section>
+<div class="owl-banner owl-carousel owl-theme fadeIn animated">
+	<a href="" class="item d-inline-block" style="background-image: url(http://via.placeholder.com/2560x690);"></a>
+</div>
