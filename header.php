1b8035f073e6855003f059c82fb4ebed2dafe17a
Web01 <fabio.gonzagabr@gmail.com>
2018-05-07 13:46:36 -0300

    bs 4.1 and owl sample page

--- header.php
+++ header.php
@@ -31,10 +31,10 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 		
 		<!-- Bootstrap 4 -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 		<!-- MMenu -->
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/jquery.mmenu.all.css">
@@ -64,11 +64,11 @@
 							<a href="<?php echo get_site_url(); ?>">
 								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/disposable/logo-.png" alt="<?php bloginfo('description'); ?>" title="<?php bloginfo('description'); ?>" id="logo-header" />
 							</a>
-							<a href="#mmenu" id="btn-mmenu" class="hidden-md-up">
+							<a href="#mmenu" id="btn-mmenu" class="d-md-block d-lg-none">
 								<i class="fas fa-bars"></i>
 							</a>
 						</div><!-- /.col-md-3 -->
-						<div class="col-md-9 py-3 hidden-sm-down">
+						<div class="col-md-9 py-3 d-none d-lg-block">
 							<div class="w-100 text-right">
 								<?php 
 									wp_nav_menu(
---
+++ page-owl-sample.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Template Name: OWL Sample
+ *
+ * The template for displaying the "Home" page.
+ *
+ * @package Visualy
+ * @since 1.0
+ */
+
+get_header(); ?>
+
+<div class="container">
+	
+	<div class="row">
+		<div class="col">
+			<!-- BANNER	 -->
+			<div class="owl-banner owl-carousel owl-theme">
+				<a href="" class="item d-inline-block" style="background-image: url(http://via.placeholder.com/2560x690);"></a>
+			</div>
+		</div>
+		<!-- /.col -->
+	</div>
+	<!-- /.row -->
+
+	<div class="spacer-5"></div>
+
+	<div class="row">
+		<!-- CAROUSEL -->
+		<div class="owl-01 owl-carousel owl-theme">
+			<div class="item">
+				<img src="http://via.placeholder.com/250x250" class="">
+			</div>
+			<!-- /.item -->
+			<div class="item">
+				<img src="http://via.placeholder.com/250x250" class="">
+			</div>
+			<!-- /.item -->
+			<div class="item">
+				<img src="http://via.placeholder.com/250x250" class="">
+			</div>
+			<!-- /.item -->
+			<div class="item">
+				<img src="http://via.placeholder.com/250x250" class="">
+			</div>
+			<!-- /.item -->
+			<div class="item">
+				<img src="http://via.placeholder.com/250x250" class="">
+			</div>
+			<!-- /.item -->
+		</div>
+		<!-- /.owl-01.owl-theme -->
+	</div>
+	<!-- /.row -->
+
+</div>
+<!-- /.container -->
+<?php get_footer(); ?>
+<script type="text/javascript">
+	
+	jQuery(document).ready(function ($) {
+		// OWL
+		jQuery('.owl-banner').owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 0,
+			autoplay: true,
+			autoplayTimeout: 7000,
+			dots: false,
+			nav: false,
+			navText: ["  "," "],
+		});
+		
+		jQuery(".owl-01").owlCarousel({
+			loop: false,
+			margin: 10,
+		 	autoplay: false,
+		 	autoplayTimeout: 7000,
+			nav: true,
+			navText: [" "," "],
+			responsive: { 
+				0 : { items: 1, },
+				320 : { items: 1, },
+				480 : { items: 2, },
+				768 : { items: 3, },
+				992 : { items: 4, },
+				1200 : { items: 4, }
+			}
+		});
+	});
+
+</script>
